478670bfcb380e222da9c56fadcbaa0cb04c3adf
Fabrigasman <fabriciogasaro@gmail.com>
2025-02-19 17:16:43 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+      let who = ["El perro", "Mi madre", "El vecino", "Mi amigo"];
+      let action = ["Comió", "Rompió", "Robó", "Dañó"];
+      let what = ["mi tarea", "mi teléfono", "mi coche"];
+      let when = [
+          "antes de la clase",
+          "cuando estaba durmiendo",
+          "mientras hacía ejercicio",
+          "durante mi almuerzo",
+          "mientras estaba rezando"
+      ];
+      let excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
+      document.getElementById("textoExcusa").innerText = excuse;
+  }
+  generateExcuse();
 };
